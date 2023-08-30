499e741f257ba77bb36175bcb02e2ade2b21e6fb
Yurii Skrynnykov <truef1s7@gmail.com>
2023-08-31 01:19:21 +0300

    Update volume.sh

--- volume.sh
+++ volume.sh
@@ -2,4 +2,4 @@
 
 # amixer get Master | tail -1 | cut -d' ' -f7 | sed -e 's/[][]//g'
 # pamixer --get-volume-human
-pactl get-sink-volume 0 | head -1 | cut -d'/' -f2,4 | sed 's/\s//g'
+pactl get-sink-volume 0 | head -1 | cut -d'/' -f2,4 | sed 's/\s//g' | tr -d '\n'
