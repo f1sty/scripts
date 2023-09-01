c9236e583abbdf83ad17f6dab015c29b4643764e
Yurii Skrynnykov <truef1s7@gmail.com>
2023-09-02 01:15:09 +0300

    Update cmus-tagline.sh script

--- cmus-tagline.sh
+++ cmus-tagline.sh
@@ -1,7 +1,8 @@
 #!/bin/env bash
 
-output=$(cmus-remote -Q 2>/dev/null | awk '/tag|status/ {print "["$2":", $3"]"}' | tr '\n' ' ')
-tagline=($output)
+output=$(cmus-remote -Q 2>/dev/null)
+
+readarray -t tagline <<< $output
 playing_status=${tagline[0]}
 
 case $playing_status in
@@ -18,4 +19,33 @@ case $playing_status in
     status_glyph="❌"
     ;;
 esac
-echo $status_glyph ${tagline[@]:2}
+
+for line in "${tagline[@]}"; do
+  args=($line)
+  case $line in
+    *tracknumber*)
+      tracknumber=${args[2]}
+      ;;
+    *title*)
+      title=${args[@]:2}
+      ;;
+    *artist*)
+      artist=${args[@]:2}
+      ;;
+    *album*)
+      album=${args[@]:2}
+      ;;
+    *date*)
+      date=${args[2]}
+      ;;
+    *comment*)
+      comment=${args[@]:2}
+      ;;
+  esac
+done
+
+if [ $status_glyph == "❌" ] || [ $status_glyph == "⏹️" ]; then
+  echo $status_glyph
+else
+  echo "$status_glyph $artist - $title [$album $date]"
+fi
