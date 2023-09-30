91fe3a5e85b7975a5b2b572c5b22ea66316991c1
Yurii Skrynnykov <truef1s7@gmail.com>
2023-09-30 22:06:55 +0300

    Fix dotme.sh script

--- dotme.sh
+++ dotme.sh
@@ -5,9 +5,11 @@ set -xe
 DOTFILES="$HOME/pkgs"
 DOTFILES_REPO="https://github.com/f1sty/dotfiles"
 
+function dotfiles() {
+  git --git-dir=$DOTFILES/dotfiles --work-tree=$HOME "$@"
+}
+
 mkdir -p $DOTFILES
 cd $DOTFILES
 git clone --bare $DOTFILES_REPO dotfiles
-alias dotfiles="git --git-dir=$DOTFILES/dotfiles --work-tree=$HOME"
 dotfiles config status.showUntrackedFiles no
-dotfiles checkout main
