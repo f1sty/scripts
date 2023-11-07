76ff7f2bc9e24b830493b43ed9d69d468b891a33
Yurii Skrynnykov <truef1s7@gmail.com>
2023-11-07 23:54:09 +0200

    Switch screenshot tool back to flameshot

--- screenshot.sh
+++ screenshot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-grim -g "$(slurp)" $HOME/media/images/screenshots/$(date +'%F_%s.png')
-# flameshot gui
+# grim -g "$(slurp)" $HOME/media/images/screenshots/$(date +'%F_%s.png')
+flameshot gui
 # scrot -is '%Y-%m-%d_%T_$wx$h.png' -e 'optipng $f && mv $f ~/media/images/screenshots/'
