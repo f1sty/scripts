5832ad6c6b04c28084dc9b50b23484bea0bebe70
Yurii Skrynnykov <truef1s7@gmail.com>
2023-11-07 23:30:36 +0200

    Fix wayland setup script

--- wl-setup.sh
+++ wl-setup.sh
@@ -1,35 +1,29 @@
 #!/usr/bin/env bash
 
-
-setup_tools() {
-  set -xe
-  shopt -s expand_aliases
-
-  source $HOME/.config/bash/aliases
-  source $HOME/.config/bash/functions
-
-  pm -S wayland swaybg foot bemenu-wayland xorg-xwayland seatd wlroots xdg-desktop-portal{-gtk,-wlr,-xapp} ydotool grim slurp wl-clipboard gammastep
-  sudo usermod -aG seat $USER 
-  sudo usermod -aG input $USER
-  sudo systemctl enable seatd
-}
-
 usage() {
   echo "$0 <dwl|sway>"
+}
+
+setup_tools() {
+    set -xe
+    sudo pacman -S wayland swaybg foot bemenu-wayland xorg-xwayland seatd wlroots xdg-desktop-portal{-gtk,-wlr,-xapp} ydotool grim slurp wl-clipboard gammastep imv
+    sudo usermod -aG seat $USER 
+    sudo usermod -aG input $USER
+    sudo systemctl enable seatd
 }
 
 case $1 in
   dwl)
     setup_tools
-    pm -S waybar 
-    pk
+    sudo pacman -S waybar 
+    cd $HOME/pkgs
     gh repo clone dwl
-    pk dwl
+    cd $HOME/pkgs/dwl
     make && make PREFIX=~/.local install clean
     ;;
   sway)
     setup_tools
-    pm -S sway
+    sudo pacman -S sway
     ;;
   *)
     usage
