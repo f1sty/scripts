a19540f4e1d49853a39775817f25e814472e41dc
Yurii Skrynnykov <truef1s7@gmail.com>
2023-12-03 20:48:43 +0200

    Update tmux start script

--- tmux-session.sh
+++ tmux-session.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-tmux attach -t base || tmux new -s base
+function new_session() {
+  tmux new-session -d -n aux -s base
+  tmux new-window -n coding
+  tmux new-window -n dbg
+  tmux new-window -n test
+  tmux send-keys -t 2 "cd $(pass work/directory)" Enter
+  tmux send-keys -t 3 "cd $(pass work/directory)" Enter
+  tmux select-window -t "base:aux"
+  tmux attach-session -d
+}
+
+tmux attach -t base || new_session
