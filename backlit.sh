a6eb7a86c727aed94ccdffe9eb57375e25e67857
Yurii Skrynnykov <truef1s7@gmail.com>
2023-12-18 19:03:04 +0200

    Update backlight script

--- backlit.sh
+++ backlit.sh
@@ -1,9 +1,40 @@
 #!/usr/bin/env bash
+# Copyright © 2023 Yurii <f1sty> Skrynnykov. All Rights Reserved.
 
 KBD_BACKLIGHT="/sys/class/leds/:white:kbd_backlight"
+SCREEN_BACKLIGHT="/sys/class/backlight/gmux_backlight"
+MAX_KBD_BRIGHTNESS=$(cat "${KBD_BACKLIGHT}/max_brightness")
+MAX_SCREEN_BRIGHTNESS=$(cat "${SCREEN_BACKLIGHT}/max_brightness")
+STEP=2
+
+kbd_brightness() {
+  echo $((${MAX_SCREEN_BRIGHTNESS} * $1 / 100))
+}
+
+screen_brightness() {
+  CURRENT_BRIGHTNESS=$(cat "${SCREEN_BACKLIGHT}/brightness")
+  ADJUST_VALUE=$((${MAX_SCREEN_BRIGHTNESS} * ${STEP} / 100))
+  case $1 in
+    up)
+      echo $((CURRENT_BRIGHTNESS + ADJUST_VALUE)) ;;
+    down)
+      echo $((CURRENT_BRIGHTNESS - ADJUST_VALUE));;
+    *)
+      echo $((${MAX_SCREEN_BRIGHTNESS} * $1 / 100));;
+  esac
+}
+
 case $1 in
-  on)
-    cat "${KBD_BACKLIGHT}/max_brightness" | sudo tee "${KBD_BACKLIGHT}/brightness";;
-  off)
-    echo 0 | sudo tee "${KBD_BACKLIGHT}/brightness";;
+  kbd_on)
+    kbd_brightness 80 | sudo tee "${KBD_BACKLIGHT}/brightness";;
+  kbd_off)
+    kbd_brightness 0 | sudo tee "${KBD_BACKLIGHT}/brightness";;
+  screen_dim)
+    screen_brightness 20 | sudo tee "${SCREEN_BACKLIGHT}/brightness";;
+  screen_full)
+    screen_brightness 100 | sudo tee "${SCREEN_BACKLIGHT}/brightness";;
+  screen_up)
+    screen_brightness up | sudo tee "${SCREEN_BACKLIGHT}/brightness";;
+  screen_down)
+    screen_brightness down | sudo tee "${SCREEN_BACKLIGHT}/brightness";;
 esac
