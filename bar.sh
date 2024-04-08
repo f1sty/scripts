37306c37fde5dde88cc7c0d7ea3ee4256874a8a5
Yurii Skrynnykov <truef1s7@gmail.com>
2024-04-08 23:35:39 +0300

    Add bar script

---
+++ bar.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+while [[ true ]]; do
+  battery="$(cat /sys/class/power_supply/BAT0/capacity)%"
+  volume="$(pactl get-sink-volume `pactl get-default-sink` | cut -d' ' -f 6,14,16)"
+  time="$(date +'%b %d/%m/%y %A %H:%M:%S')"
+
+  xsetroot -name "🔊 $volume | 🔋 $battery | $time"
+  sleep 1 
+done
