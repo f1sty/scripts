ce1a8bbf4a5dae208579fa60ac0c8a578fabe557
Yurii Skrynnykov <truef1s7@gmail.com>
2024-05-09 00:45:16 +0300

    Update tmux-session script

--- tmux-session.sh
+++ tmux-session.sh
@@ -2,13 +2,13 @@
 
 function new_session() {
   tmux new-session -d -n coding -s base
-  tmux new-window -n test
   tmux new-window -n dbg
+  tmux new-window -n test
   tmux new-window -n cmd
   tmux new-window -n aux
-  tmux send-keys -t 1 "cd $(pass work/directory)" Enter
-  tmux send-keys -t 2 "cd $(pass work/directory)" Enter
-  tmux send-keys -t 3 "cd $(pass work/directory)" Enter
+  tmux send-keys -t "base:coding" "cd $(pass work/directory)" Enter
+  tmux send-keys -t "base:dbg" "cd $(pass work/directory)" Enter
+  tmux send-keys -t "base:test" "cd $(pass work/directory)" Enter
   tmux select-window -t "base:coding"
   tmux attach-session -d
 }
